6e1096ab8bd9110e396a47ab77965298ca574d9d
Le-Bennou <bnjmgraph@gmail.com>
2025-05-23 18:38:40 +0200

    correction typo

--- src/core/system/base/controllerTemplate.js
+++ src/core/system/base/controllerTemplate.js
@@ -2,7 +2,7 @@ import { abstractController } from '$path$core/system/base/abstractController.js
 
 /**
  * @typedef {import('./M_Config.cjs').$ComponentName$_Model} $ComponentName$_Model
- * @extends {abstractController<C$ComponentName$_Model>}
+ * @extends {abstractController<$ComponentName$_Model>}
  */
 export class $ComponentName$_Controller extends abstractController{
 
