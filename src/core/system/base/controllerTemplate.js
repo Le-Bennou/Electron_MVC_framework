5be83c0d334bd637f472ebe7885c3753114d1630
Le-Bennou <bnjmgraph@gmail.com>
2025-05-23 18:39:13 +0200

    corection jsdoc

--- src/core/system/base/controllerTemplate.js
+++ src/core/system/base/controllerTemplate.js
@@ -1,7 +1,7 @@
 import { abstractController } from '$path$core/system/base/abstractController.js';
 
 /**
- * @typedef {import('./M_Config.cjs').$ComponentName$_Model} $ComponentName$_Model
+ * @typedef {import('./M_$ComponentName$.cjs').$ComponentName$_Model} $ComponentName$_Model
  * @extends {abstractController<$ComponentName$_Model>}
  */
 export class $ComponentName$_Controller extends abstractController{
